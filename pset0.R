8f70c34e6893b47def1d5d4c551751355cdd576d
bzsparks98 <bsparks@stanford.edu>
2022-09-30 14:11:24 -0700

    Added Credits

--- pset0.R
+++ pset0.R
@@ -2,3 +2,6 @@ print("I'm good, yeah, I'm feelin' alright
 Baby, I'ma have the best dang night of my life
 And wherever it takes me, I'm down for the ride
 Baby, don't you know I'm good? Yeah, I'm feelin' alright")
+print("Thank you for listening!")
+print("Songwriters: David Guetta / Gianfranco Randone / Massimo Gabutti / Maurizio Lobina / Philip Plested / Camille Purcell / Bleta Rexha
+I'm Good (Blue) lyrics © Hotel Cabana Limited, Jackback Publishing Limited")
