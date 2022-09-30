512abc18231d9334933f46a4e8de3407daf71f22
bzsparks98 <bsparks@stanford.edu>
2022-09-30 14:07:29 -0700

    initial commit of pset0.R

---
+++ pset0.R
@@ -0,0 +1,4 @@
+print("I'm good, yeah, I'm feelin' alright
+Baby, I'ma have the best dang night of my life
+And wherever it takes me, I'm down for the ride
+Baby, don't you know I'm good? Yeah, I'm feelin' alright")
